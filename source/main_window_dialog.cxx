84b385dc1c16a89afdcdf6ac73427f6408cfd72f
PsychedelicShayna <0x0000@tuta.io>
2022-03-26 10:37:58 -0300

    Laid out the foundation for cursor-rails

--- source/main_window_dialog.cxx
+++ source/main_window_dialog.cxx
@@ -1,12 +1,61 @@
 #include "main_window_dialog.hxx"
 #include "ui_main_window_dialog.h"
 
+void MainWindow::registerHorizontalRailHotkey(HotkeyRecorderWidget::Hotkey hotkey) {
+
+}
+
+void MainWindow::registerVerticalRailHotkey(HotkeyRecorderWidget::Hotkey hotkey) {
+
+}
+
+void MainWindow::on_spinBoxHorizontalHotkeyVkid_valueChanged(int) {
+
+}
+
+void MainWindow::on_spinBoxVerticalHotkeyVkid_valueChanged(int) {
+
+}
+
+bool MainWindow::nativeEvent(const QByteArray& event_type, void* message, qintptr* result) {
+
+
+    QMainWindow::nativeEvent(event_type, message, result);
+}
+
 MainWindow::MainWindow(QWidget* parent)
     :
-      QMainWindow    { parent             },
-      ui             { new Ui::MainWindow }
+      QMainWindow                     { parent                            },
+      ui                              { new Ui::MainWindow                },
+      hotkeyRecorderHorizontalRail    { new HotkeyRecorderWidget { this } },
+      hotkeyRecorderVerticalRail      { new HotkeyRecorderWidget { this } },
+      horizontalRailHotkeyId          { 0x41414141                        },
+      verticalRailHotkeyId            { 0x42424242                        }
 {
     ui->setupUi(this);
+
+    setWindowFlags(
+                Qt::Dialog
+                | Qt::CustomizeWindowHint
+                | Qt::WindowTitleHint
+                | Qt::WindowCloseButtonHint
+                | Qt::WindowMinimizeButtonHint
+                | Qt::WindowMaximizeButtonHint
+                );
+
+    ui->horizontalLayoutHotkeyRecorders->addWidget(hotkeyRecorderHorizontalRail);
+    ui->horizontalLayoutHotkeyRecorders->addWidget(hotkeyRecorderVerticalRail);
+
+    connect(hotkeyRecorderHorizontalRail, SIGNAL(HotkeyRecorded(HotkeyRecorderWidget::WindowsHotkey)),
+            this,                         SLOT(registerHorizontalRailHotkey));
+
+    connect(hotkeyRecorderVerticalRail,   SIGNAL(HotkeyRecorded(HotkeyRecorderWidget::WindowsHotkey)),
+            this,                         SLOT(registerVerticalRailHotkey));
+
+    hotkeyRecorderHorizontalRail->StartRecording();
+    hotkeyRecorderVerticalRail->StartRecording();
+
+    setMaximumHeight(height());
 }
 
 MainWindow::~MainWindow() {
--- source/main_window_dialog.hxx
+++ source/main_window_dialog.hxx
@@ -14,8 +14,19 @@ Q_OBJECT
 private:
     Ui::MainWindow* ui;
 
+    HotkeyRecorderWidget* hotkeyRecorderHorizontalRail, *hotkeyRecorderVerticalRail;
+    uint32_t              horizontalRailHotkeyId,        verticalRailHotkeyId;
+
+    Q_SLOT void registerHorizontalRailHotkey(HotkeyRecorderWidget::Hotkey hotkey);
+    Q_SLOT void registerVerticalRailHotkey(HotkeyRecorderWidget::Hotkey hotkey);
+
+    Q_SLOT void on_spinBoxHorizontalHotkeyVkid_valueChanged(int);
+    Q_SLOT void on_spinBoxVerticalHotkeyVkid_valueChanged(int);
+
+    virtual bool nativeEvent(const QByteArray& event_type, void* message, qintptr* result) override;
+
 public:
-    MainWindow(QWidget *parent = nullptr);
+    MainWindow(QWidget* parent = nullptr);
     virtual ~MainWindow() override;
 };
 
